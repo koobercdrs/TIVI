25f448e6fdaf7198e4ddbc7e167917154951424c
gochazautashvili <gochazautashvili2017@gmail.com>
2025-12-23 07:24:37 +0400

    feat: remove console logs from getEventById function for cleaner code

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -3,12 +3,9 @@ import { getPayload } from '@/library/payload'
 import { ILang } from '@/types'
 
 export const getEventById = async (id: string, lang: ILang) => {
-  console.log(id)
-
   const payload = await getPayload()
 
   const content = await payload.findByID({ id: id, collection: 'events', locale: lang })
-  console.log(content)
 
   return content
 }

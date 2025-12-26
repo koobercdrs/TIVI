09e28e9c831fe706acb3b9fbbff75c7e22b7cbf1
gochazautashvili <gochazautashvili2017@gmail.com>
2025-12-26 05:25:11 +0400

    fix

--- src/components/sections/contact/index.tsx
+++ src/components/sections/contact/index.tsx
@@ -6,64 +6,80 @@ export const Contact = ({ content }: { content: HomeView['contact'] }) => {
   return (
     <div className={styles.section_bg}>
       <section id="CONTACT" className={`${styles.section} container`}>
-        <div className={styles.content}>
-          <div className={styles.info}>
-            <h1 className={styles.title}>{content.header.title}</h1>
-            <p className={styles.text}>{content.header.description}</p>
-          </div>
-
-          <div className={styles.wrapper}>
-            <div className={styles.card}>
-              <Image draggable={false} src="/contact/Phone.svg" alt="icon" width={32} height={32} />
-              <span>{content.info.phone}</span>
-            </div>
-            <div className={styles.card}>
-              <Image
-                draggable={false}
-                src="/contact/Everyday.svg"
-                alt="icon"
-                width={32}
-                height={32}
-              />
-              <span>{content.info.working_hours}</span>
-            </div>
-            <div className={styles.card}>
-              <Image
-                draggable={false}
-                src="/contact/Location.svg"
-                alt="icon"
-                width={32}
-                height={32}
-              />
-              <span>{content.info.address}</span>
+        <div className={styles.content_wrapper}>
+          <div className={styles.content}>
+            <div className={styles.info}>
+              <h1 className={styles.title}>{content.header.title}</h1>
+              <p className={styles.text}>{content.header.description}</p>
             </div>
 
-            <div className={styles.social}>
-              <a href={content.social.facebook || ''} className={styles.card}>
+            <div className={styles.wrapper}>
+              <div className={styles.card}>
                 <Image
                   draggable={false}
-                  src="/contact/Facebook.svg"
-                  height={32}
-                  width={32}
+                  src="/contact/Phone.svg"
                   alt="icon"
+                  width={32}
+                  height={32}
                 />
-
-                <span>Facebook</span>
-              </a>
-
-              <a href={content.social.instagram || ''} className={styles.card}>
+                <span>{content.info.phone}</span>
+              </div>
+              <div className={styles.card}>
                 <Image
-                  src="/contact/Instagram.svg"
                   draggable={false}
-                  height={32}
-                  width={32}
+                  src="/contact/Everyday.svg"
                   alt="icon"
+                  width={32}
+                  height={32}
                 />
+                <span>{content.info.working_hours}</span>
+              </div>
+              <div className={styles.card}>
+                <Image
+                  draggable={false}
+                  src="/contact/Location.svg"
+                  alt="icon"
+                  width={32}
+                  height={32}
+                />
+                <span>{content.info.address}</span>
+              </div>
 
-                <span>Instagram</span>
-              </a>
+              <div className={styles.social}>
+                <a href={content.social.facebook || ''} className={styles.card}>
+                  <Image
+                    draggable={false}
+                    src="/contact/Facebook.svg"
+                    height={32}
+                    width={32}
+                    alt="icon"
+                  />
+
+                  <span>Facebook</span>
+                </a>
+
+                <a href={content.social.instagram || ''} className={styles.card}>
+                  <Image
+                    src="/contact/Instagram.svg"
+                    draggable={false}
+                    height={32}
+                    width={32}
+                    alt="icon"
+                  />
+
+                  <span>Instagram</span>
+                </a>
+              </div>
             </div>
           </div>
+
+          <Image
+            className={styles.content_wrapper_bg}
+            src="/svgs/5.svg"
+            height={400}
+            width={600}
+            alt="svg"
+          />
         </div>
 
         <div className={styles.map}>
